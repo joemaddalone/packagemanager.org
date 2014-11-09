b91783082de093b2f0d709d01772ea873603a352
Joe Maddalone <joe@insert-title.com>
2014-11-09 11:53:01 -0600

    updated component url, added homebrew

--- js/app/Packages.js
+++ js/app/Packages.js
@@ -9,10 +9,11 @@ define([], function(){
     {'title':'ruby gems','description':'RubyGems.org is the Ruby community\'s gem hosting service','img':'rubygems.png','url':'http://rubygems.org/'},
     {'title':'jam','description':'Jam is a package manager for JavaScript. Unlike other repositories, we put the browser first.','img':'jam.png','url':'http://www.jamjs.org'},
     {'title':'Yeoman','description':'Need a dependency? It\'s just a keystroke away. [uses bower]','img':'yeoman.png','url':'http://yeoman.io/'},
-    {'title':'Component','description':'Component package manager for building a better web.','img':'component.png','url':'http://component.io/'},
+    {'title':'Component','description':'Component package manager for building a better web.','img':'component.png','url':'http://component.github.io/'},
     {'title':'Sublime PC','description':'A package manager for Sublime Text 2.','img':'sublime.png','url':'http://wbond.net/sublime_packages/package_control'},
     {'title':'Nuget','description':'NuGet is a Visual Studio package manager.','img':'nuget.png','url':'http://wbond.net/sublime_packages/package_control'},
     {'title':'pub','description':'Explore packages here and install them using pub, the package manager for Dart.','img':'pub.png','url':'http://pub.dartlang.org'},
-    {'title':'Vagrant','description':'It\'s a package manager, but the packages are VMs... crazy, right?.','img':'vagrant.png','url':'http://www.vagrantup.com/'}
+    {'title':'Vagrant','description':'It\'s a package manager, but the packages are VMs... crazy, right?.','img':'vagrant.png','url':'http://www.vagrantup.com/'},
+    {'title':'Homebrew','description':'The missing package manager for OS X','img':'homebrew.png','url':'http://brew.sh/'}
   ];
 });
